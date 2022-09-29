604521ff2baa577b1eb910c0b8311f22a883fef1
Charith Jayawardana <cvljayawardana@gmail.com>
2022-09-29 12:45:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import search
+
+
+if __name__ == "__main__":
+    print(search.google_make_request())
+    print(search.bing_make_request())
---
+++ search.py
@@ -0,0 +1,27 @@
+import requests
+import constants
+
+# https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+
+
+def google_make_request():
+    params = {
+        'key': constants.GOOGLE_API_KEY,
+        'cx': 'a3307a6a039e048b0',
+        'q': 'charith jayawardana'
+    }
+    response = requests.get(constants.GOOGLE_API_HOST, params=params)
+    jsonFormat = response.json()
+    # for i in jsonFormat:
+    #     print("key: ", i, "val: ", jsonFormat[i])
+    return jsonFormat['queries']['request'][0]['totalResults']
+
+
+def bing_make_request():
+    headers = {"Ocp-Apim-Subscription-Key": constants.BING_API_KEY}
+    params = {"q": ' charith jayawardana',
+              "textDecorations": True, "textFormat": "HTML"}
+    response = requests.get(constants.BING_API_HOST,
+                            params=params, headers=headers)
+    jsonFormat = response.json()
+    return jsonFormat['webPages']['totalEstimatedMatches']
