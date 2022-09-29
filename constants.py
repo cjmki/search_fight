bba36acfeddf60ac3d55b5b2b732c4c57791fc91
Charith Jayawardana <cvljayawardana@gmail.com>
2022-09-29 12:56:51 +0200

    lint: linter with pre commit hooks intergrated

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-GOOGLE_API_HOST = 'https://www.googleapis.com/customsearch/v1'
-BING_API_HOST = 'https://api.bing.microsoft.com/v7.0/search'
+GOOGLE_API_HOST = "https://www.googleapis.com/customsearch/v1"
+BING_API_HOST = "https://api.bing.microsoft.com/v7.0/search"
 # cse - https://support.google.com/programmable-search/answer/70392
-GOOGLE_API_KEY = 'AIzaSyAgswpDc2zIDS5mL0uLtVI0PIKeFZN5KzY'
-BING_API_KEY = 'ee4e459ba5684362a17dd7b749113f89'
+GOOGLE_API_KEY = "AIzaSyAgswpDc2zIDS5mL0uLtVI0PIKeFZN5KzY"
+BING_API_KEY = "ee4e459ba5684362a17dd7b749113f89"
--- search.py
+++ search.py
@@ -6,22 +6,26 @@ import constants
 
 def google_make_request():
     params = {
-        'key': constants.GOOGLE_API_KEY,
-        'cx': 'a3307a6a039e048b0',
-        'q': 'charith jayawardana'
+        "key": constants.GOOGLE_API_KEY,
+        "cx": "a3307a6a039e048b0",
+        "q": "charith jayawardana",
     }
     response = requests.get(constants.GOOGLE_API_HOST, params=params)
     jsonFormat = response.json()
     # for i in jsonFormat:
     #     print("key: ", i, "val: ", jsonFormat[i])
-    return jsonFormat['queries']['request'][0]['totalResults']
+    return jsonFormat["queries"]["request"][0]["totalResults"]
 
 
 def bing_make_request():
     headers = {"Ocp-Apim-Subscription-Key": constants.BING_API_KEY}
-    params = {"q": ' charith jayawardana',
-              "textDecorations": True, "textFormat": "HTML"}
-    response = requests.get(constants.BING_API_HOST,
-                            params=params, headers=headers)
+    params = {
+        "q": " charith jayawardana",
+        "textDecorations": True,
+        "textFormat": "HTML",
+    }
+    response = requests.get(
+        constants.BING_API_HOST, params=params, headers=headers
+    )
     jsonFormat = response.json()
-    return jsonFormat['webPages']['totalEstimatedMatches']
+    return jsonFormat["webPages"]["totalEstimatedMatches"]
